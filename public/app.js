b9f137501fa291ca6dc6195ad0ebded07c14372c
zahra khivveh <zahrakhivveh@gmail.com>
2025-12-31 23:39:39 +0330

    1404/10/10

--- public/app.js
+++ public/app.js
@@ -1,10 +1,15 @@
-const toggleThemeBtn = document.querySelector("#toggle-theme");
-toggleThemeBtn.addEventListener("click" , () => {
-    if (localStorage.theme === "dark"){
-        document.documentElement.classList.remove("dark");
-        localStorage.theme = "light";
-    } else {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("theme" , "dark");
-    }
+const toggleThemeBtn = document.querySelectorAll(".toggle-theme");
+
+toggleThemeBtn.forEach(btn => {
+
+    btn.addEventListener("click" , function() {
+        if (localStorage.theme === "dark"){
+            document.documentElement.classList.remove("dark");
+            localStorage.theme = "light";
+        } else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme" , "dark");
+        }
+})
+
 })
